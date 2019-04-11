b68de8f5cc45f5075572d6b948fd29b40bd003f8
Albert Abdulmanov <albert.abdu@gmail.com>
2019-04-11 05:14:47 +0500

    Updates

---
+++ precache-manifest.f243901826d0ed7318c531989d93db9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5edbb2f651f71329e522",
+    "url": "/static/js/main.de6f86e3.chunk.js"
+  },
+  {
+    "revision": "45d1db89bcefd78eed75",
+    "url": "/static/js/2.c42eaf27.chunk.js"
+  },
+  {
+    "revision": "5edbb2f651f71329e522",
+    "url": "/static/css/main.3fd8be4d.chunk.css"
+  },
+  {
+    "revision": "fa1fd898b25915342be69d5334f61fba",
+    "url": "/index.html"
+  }
+];
